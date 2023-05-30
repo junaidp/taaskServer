7227c17f58891a0ed785b9cfbb4e332c862ff442
hassam.shah <hassamshah1997@gmail.com>
2023-05-30 22:27:56 +0500

    userprofile

--- src/main/java/com/echo/taask/helper/UserHelper.java
+++ src/main/java/com/echo/taask/helper/UserHelper.java
@@ -43,77 +43,80 @@ public class UserHelper {
     MongoOperations mongoOperations;
 
     Gson gson = new Gson();
-    public List<User> getAllUsers(){
-        try{
+
+    public List<User> getAllUsers() {
+        try {
             return userRepository.findAll();
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             throw e;
         }
     }
 
-    public String saveUser(User user){
+    public User getUserByEmail(User user) {
+        try {
+            if (user.getEmail()!=null) {
+                return userRepository.findByEmail(user.getEmail());
+            }else
+            {
+                return null;
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public User saveUser(User user) {
         try {
             //String password = user.getPassword();
             userRepository.save(user);
-            return "user saved";
-        }catch (Exception e)
-        {
+            return user;
+        } catch (Exception e) {
             throw e;
         }
     }
 
-    public String deleteAllUsers()
-    {
-        try{
+    public String deleteAllUsers() {
+        try {
             userRepository.deleteAll();
             return "All users Deleted";
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw e;
         }
     }
 
-    public ResponseEntity<User> login(String userName , String password){
-        try{
+    public ResponseEntity<User> login(String userName, String password) {
+        try {
             Query query = new Query();
             query.addCriteria(Criteria.where("email").is(userName));
             query.addCriteria(Criteria.where("password").is(password));
-            User user = mongoOperations.findOne(query,User.class);
-            if(user!= null)
-                return new ResponseEntity<>(user ,HttpStatus.OK);
+            User user = mongoOperations.findOne(query, User.class);
+            if (user != null)
+                return new ResponseEntity<>(user, HttpStatus.OK);
             else
                 return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw e;
         }
     }
 
-    public String updateUser(User user)
-    {
-        try{
+    public String updateUser(User user) {
+        try {
             Query query = new Query();
             query.addCriteria(Criteria.where("id").is(user.getId()));
-            User updateuser = mongoOperations.findOne(query,User.class);
-            if(updateuser.equals(null))
-            {
+            User updateuser = mongoOperations.findOne(query, User.class);
+            if (updateuser.equals(null)) {
                 return "could not find user";
-            }
-            else {
+            } else {
                 userRepository.save(user);
                 return "Updated Successfully";
             }
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             throw e;
         }
     }
 
-    public String cleanDb(){
+    public String cleanDb() {
         try {
             customerRepository.deleteAll();
             contactsRepository.deleteAll();
@@ -121,9 +124,8 @@ public class UserHelper {
             taskRepository.deleteAll();
             meetingRepository.deleteAll();
             return "Db Cleaned";
-        }catch(Exception ex)
-        {
-            return "Error in cleaning db: "+ ex.getMessage();
+        } catch (Exception ex) {
+            return "Error in cleaning db: " + ex.getMessage();
         }
     }
 }
